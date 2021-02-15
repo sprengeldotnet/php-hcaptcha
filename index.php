5ba9020176b93cf001346353621478e088c77d50
sprengel <66063434+sprengeldotnet@users.noreply.github.com>
2021-02-15 13:18:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,26 +19,14 @@ if(isset($_POST["submit"])){
   $data = array('secret' => '0x0000000000000000000000000000000000000000', 'response' => $_POST['h-captcha-response']);
   //replace the dummy key with your secret key
   
-  $ch = curl_init();
-  //Initialize cURL session
-  
-  curl_setopt($ch, CURLOPT_URL, 'https://hcaptcha.com/siteverify');
-  //Set Verify URL
-  
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-  //Set the Data to Post
-  
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  //do not output, use variable
-  
-  $response = curl_exec($ch);
-  //store the content in variable
-  
-  $json_response = json_decode($response, true);
-  //decode the response
-  
-  curl_close($ch);
-  //close curl
+  //don't touch this section, i somehow got it to work.
+  $ch = curl_init();  //Initialize cURL session
+  curl_setopt($ch, CURLOPT_URL, 'https://hcaptcha.com/siteverify'); //Set Verify URL
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);  //Set the Data to Post
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //do not output, use variable
+  $response = curl_exec($ch); //store the content in variable
+  $json_response = json_decode($response, true);  //decode the response
+  curl_close($ch);  //close curl
   
   if ($response['success']){ 
   //if true ( {"success":true,[...]} )
