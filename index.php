b1b00390c101418b379035e0e1fac5d01a48bad0
sprengel <66063434+sprengeldotnet@users.noreply.github.com>
2021-04-07 11:06:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if(isset($_POST["submit"])){
     
     // do stuff
     
-  } else{
+  } else {
   //if response is not true ( {"success":false,[...]} )
     
     print("Please verify the captcha to continue");
